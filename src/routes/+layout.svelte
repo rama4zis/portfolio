4d5524d03bfbf51b56d8cf5e77ce7b018feb57b7
rama <rama.4ditya3@gmail.com>
2025-07-21 22:50:26 +0700

    Refactor code structure for improved readability and maintainability

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  let { children } = $props();
+  import "../app.css";
+</script>
+
+<div
+  class="min-h-screen bg-gradient-to-t from-gray-700 via-gray-900 to-black"
+>
+  {@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div
+  class="h-screen min-w-full mx-auto py-12 flex justify-center text-gray-300"
+>
+  <div class="bg-gray-700 p-4 rounded-4xl">
+    <!-- Navigation -->
+    <!-- <div class="flex justify-center">
+      <a
+        href="http://github.com/rama4zis"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Github Profile"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+          />
+        </svg>
+      </a>
+    </div> -->
+
+    <!-- Profile -->
+     <div class="rounded-full w-24 h-24 overflow-hidden my-2">
+      <img class="w-full h-full object-cover" src="/images/profile.gif" alt="Profile picture" aria-hidden="true" />
+     </div>
+     
+     <h1 class="text-5xl font-bold">
+      Hello, I'am Rama.
+      <br />
+      A full-stack developer.
+    </h1>
+     
+
+
+
+    <h1 class="text-3xl font-bold">Hello world!</h1>
+  </div>
+</div>
